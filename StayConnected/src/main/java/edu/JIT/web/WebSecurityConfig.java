190461f582237924e0c98b662f543a7861dfb66f
spinosajoseph <spinosajoseph@gmail.com>
2018-11-13 16:29:54 -0500

    removed nicks template, updated websecurityconfig

--- StayConnected/src/main/java/edu/JIT/web/WebSecurityConfig.java
+++ StayConnected/src/main/java/edu/JIT/web/WebSecurityConfig.java
@@ -1,7 +1,55 @@
 package edu.JIT.web;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Configuration
+	@EnableWebSecurity
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Autowired
+		private DataSource dataSource;
+		
+		@Autowired
+		private PasswordEncoder passwordEncoder;
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+				.antMatchers("/", "/registration").permitAll()
+				.and()
+			.exceptionHandling().accessDeniedPage("/denied").and()
+			.formLogin()
+				.failureUrl("/login?error")
+				.defaultSuccessUrl("/registration")
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/login?logout")
+				.permitAll();
+		}
+		
+		@Override
+		public void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.jdbcAuthentication().dataSource(dataSource)
+			.passwordEncoder(passwordEncoder).usersByUsernameQuery("select email, password, true as active" + 
+			 " from account where email=?").authoritiesByUsernameQuery("select user_email , role from "
+			 		+ "authority where user_email=?");
+		}	
+		
+		@Bean(name="passwordEncoder")
+		public PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder(); }
+	}
 
-}
