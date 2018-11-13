bb3230ec619dfb763611bb3094766579c1e3c5ea
spinosajoseph <spinosajoseph@gmail.com>
2018-11-13 15:08:30 -0500

    updated pom , added web package

---
+++ StayConnected/src/main/java/edu/JIT/web/MvcConfig.java
@@ -0,0 +1,7 @@
+package edu.JIT.web;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+}
---
+++ StayConnected/src/main/java/edu/JIT/web/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package edu.JIT.web;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
