28af3e77f2592fcd5c9d076e8a96fa87c37295f1
spinosajoseph <spinosajoseph@gmail.com>
2018-11-13 13:20:51 -0500

    Made new folders, made controller

---
+++ StayConnected/src/main/java/edu/JIT/Controller/AccountController.java
@@ -0,0 +1,30 @@
+package edu.JIT.Controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import edu.JIT.model.UserAccount;
+import edu.JIT.dao.UserAccountDao;
+import edu.JIT.model.RegistrationForm;
+
+public class AccountController {
+
+	@Autowired
+	UserAccountDao dao;
+	
+	@GetMapping("/registration")
+	public String registration(RegistrationForm accountForm, Model model) {
+		model.addAttribute("accountForm" , accountForm);
+		return "registration"; }
+	
+	@PostMapping("/registration")
+	public String addAccount( @Valid RegistrationForm accountForm, BindingResult result, Model model) {
+		dao.createNewAccount(accountForm);
+		return "activateActivate";
+	}
+}
